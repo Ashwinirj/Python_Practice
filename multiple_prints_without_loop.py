950e4c092887dba0c747d0db605e0fcc67399647
ashwinirj <ashwinij859@gmail.com>
2024-08-25 23:15:47 +0530

    Updating a file

---
+++ multiple_prints_without_loop.py
@@ -0,0 +1,17 @@
+s = "Ashwini"
+
+print(f'{s}\n'*5)
+
+print("Python", "is", "super", "fun",sep="_")
+print(s,s,sep="***")
+
+
+
+# ##############################################
+
+# class Print:
+#     def __init__(self):
+#         print("Hello")
+ 
+# N = 5
+# a = [Print() for i in range(N)]
